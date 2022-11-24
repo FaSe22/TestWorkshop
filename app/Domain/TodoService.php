43f617d26c2af0ae9a344cf1ef3e609357ce781f
sebastianfaber <sebastian@startup-werk.de>
2022-11-24 15:50:27 +0100

    Implement get() Method in TodoService.php

--- app/Domain/TodoService.php
+++ app/Domain/TodoService.php
@@ -2,9 +2,15 @@
 
 namespace App\Domain;
 
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Collection;
+
 class TodoService
 {
 
-
+    public function get(): Collection
+    {
+        return Todo::all();
+    }
 
 }
