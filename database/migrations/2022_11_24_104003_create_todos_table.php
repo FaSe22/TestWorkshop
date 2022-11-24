5957230bab7b00571c84ed539e4c33617a0c8143
sebastianfaber <sebastian@startup-werk.de>
2022-11-24 12:03:20 +0100

    set length value for title to 50 in todos table

--- database/migrations/2022_11_24_104003_create_todos_table.php
+++ database/migrations/2022_11_24_104003_create_todos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title', 50);
             $table->timestamps();
         });
     }
